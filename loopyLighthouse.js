6dbef1f541b070f82284c22c2732a9c5a2b45a68
Ahmed Alani <s3leha@gmail.com>
2017-08-29 19:54:35 +0000

    finishing up the second part of loopyLighthouse

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,16 +1,17 @@
+function loopyLighthouse(range, multiples, words) {
 
-for (var i = 100; i <= 200; i++) {
+  for (var i = range[0]; i <= range[1]; i++) {
 
-  if (i % 3 === 0 && i % 4 === 0) {
-  console.log('LoopyLighthouse');
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (i % multiples[0] === 0) {
+      console.log(words[0]);
 
-} else if (i % 4 === 0){
-  console.log('Lighthouse');
-
-} else if (i % 3 === 0) {
-  console.log('Loopy');
-}
-  else {
-  console.log(i);
-}
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    }
+    else {
+      console.log(i);
+    }
+  }
 }
